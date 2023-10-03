dfdd52cef1a93230469bca9aabee1eec4267bcdd
gianluca.rizzo2 <gianluca.rizzo2@sistinf.it>
2023-10-03 15:58:10 +0200

    impl

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://matrimonio-omega.vercel.app',
         changeOrigin: true
       }
     }
